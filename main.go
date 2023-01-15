2bee97eda3bef1f8d63d7a5a551fbd289503174c
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2023-01-15 18:57:02 +0700

    formatting

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	}
 
 	for i := 0; i <= 50; i++ {
-		fmt.Println("");
+		fmt.Println("")
 	}
 	fmt.Printf("Hello to %s! This is the Monkey Programming Language from \"Writing An Interpreter in Go\"\n", user.Name)
 	fmt.Println("Feel free to try!")
