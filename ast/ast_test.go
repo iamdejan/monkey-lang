e30dea89bd15248c0432229cb75f185da7a1b5d3
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2023-01-19 12:24:19 +0700

    feat: add string representation

---
+++ ast/ast_test.go
@@ -0,0 +1,29 @@
+package ast
+
+import (
+	"monkey/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.Let, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.Identifier, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.Identifier, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	expected := "let myVar = anotherVar;"
+	if program.String() != expected {
+		t.Fatalf("wrong program.String(). expected=`%s`, actual=`%s`", expected, program.String())
+	}
+}
