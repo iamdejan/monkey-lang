db122e21168648d5c5cad68efc540593e1cebfe1
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2023-01-19 21:57:14 +0700

    feat: implement expression parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -165,3 +165,38 @@ func validateParseErrors(t *testing.T, p *Parser, expectedErrors []string) {
 }
 
 // end region utilities
+
+// region identifier expression
+
+func TestIdentifierExpression(t *testing.T) {
+	input := "foobar;"
+
+	l := lexer.NewLexer(input)
+	p := NewParser(l)
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program has not enough statements. expected=`1` statement, actual=`%d` statement(s).", len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not `*ast.ExpressionStatement`, but rather `%T`", program.Statements[0])
+	}
+
+	ident, ok := stmt.Expression.(*ast.Identifier)
+	if !ok {
+		t.Fatalf("stmt.Expression is not `*ast.Identifier`, but rather `%T`", stmt.Expression)
+	}
+
+	if ident.Value != "foobar" {
+		t.Errorf("wrong ident.Value. expected=`foobar`, actual=`%s`", ident.Value)
+	}
+
+	if ident.TokenLiteral() != "foobar" {
+		t.Errorf("wrong ident.TokenLiteral(). expected=`foobar`, actual=`%s`", ident.Value)
+	}
+}
+
+// end region identifier expression
