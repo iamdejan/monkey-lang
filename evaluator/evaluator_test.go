1c4e88d881535267fa3fd4db1db6493d39b89215
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2023-02-04 22:59:47 +0700

    feat(object): prepare object skeleton

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,22 @@
+package evaluator
+
+import "testing"
+
+type IntegerEvalTest struct {
+	input string
+	expected int64
+}
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []IntegerEvalTest{
+		{input: "5", expected: 5},
+		{input: "10", expected: 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+
