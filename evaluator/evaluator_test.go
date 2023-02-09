def0a0fc18523bd17fcfabce16add93e9932f3af
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2023-02-09 22:10:00 +0700

    feat(evaluator): evaluate prefix operators

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -11,6 +11,8 @@ func TestEvalIntegerExpression(t *testing.T) {
 	tests := []IntegerEvalTest{
 		{input: "5", expected: 5},
 		{input: "10", expected: 10},
+		{input: "-5", expected: -5},
+		{input: "-10", expected: -10},
 	}
 
 	for _, tt := range tests {
@@ -28,6 +30,27 @@ func TestEvalBooleanExpression(t *testing.T) {
 	tests := []BoolEvalTest{
 		{input: "true", expected: true},
 		{input: "false", expected: false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+type BangOperatorTest struct {
+	input    string
+	expected bool
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []BangOperatorTest{
+		{input: "!true", expected: false},
+		{input: "!false", expected: true},
+		{input: "!5", expected: false},
+		{input: "!!true", expected: true},
+		{input: "!!false", expected: false},
+		{input: "!!5", expected: true},
 	}
 
 	for _, tt := range tests {
