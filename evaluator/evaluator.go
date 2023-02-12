0d314f61acfa1da34d584431bc635fb705962a21
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2023-02-12 23:43:31 +0700

    feat(evaluator): evaluate infix

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -18,6 +18,10 @@ func Eval(node ast.Node) object.Object {
 	case *ast.PrefixExpression:
 		right := Eval(node.Right)
 		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 	case *ast.IntegerLiteral:
@@ -77,4 +81,47 @@ func evalMinusPrefixOperatorExpression(right object.Object) object.Object {
 
 	value := right.(*object.Integer).Value
 	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	switch {
+	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
+		return evalIntegerInfixExpression(operator, left, right)
+	case operator == "==":
+		return nativeToBooleanObject(left == right)
+	case operator == "!=":
+		return nativeToBooleanObject(left != right)
+	default:
+		return NULL
+	}
+}
+
+func evalIntegerInfixExpression(operator string, left object.Object, right object.Object) object.Object {
+	leftVal := left.(*object.Integer).Value
+	rightVal := right.(*object.Integer).Value
+
+	switch operator {
+	case "+":
+		return &object.Integer{Value: leftVal + rightVal}
+	case "-":
+		return &object.Integer{Value: leftVal - rightVal}
+	case "*":
+		return &object.Integer{Value: leftVal * rightVal}
+	case "/":
+		return &object.Integer{Value: leftVal / rightVal}
+	case "==":
+		return nativeToBooleanObject(leftVal == rightVal)
+	case "!=":
+		return nativeToBooleanObject(leftVal != rightVal)
+	case "<":
+		return nativeToBooleanObject(leftVal < rightVal)
+	case "<=":
+		return nativeToBooleanObject(leftVal <= rightVal)
+	case ">":
+		return nativeToBooleanObject(leftVal > rightVal)
+	case ">=":
+		return nativeToBooleanObject(leftVal >= rightVal)
+	default:
+		return NULL
+	}
 }
