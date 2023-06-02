671cdd88452d162bfef0c0df8e24311a37d6b8cc
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2023-06-02 22:47:16 +0700

    feat(evaluator): evaluate array literal and index (#35)

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -320,3 +320,79 @@ func TestBuiltInFunctions(t *testing.T) {
 		}
 	}
 }
+
+func TestArrayLiterals(t *testing.T) {
+	input := `[1, 2 * 2, 3 + 3]`
+
+	evaluated := testEval(input)
+	result, ok := evaluated.(*object.Array)
+	if !ok {
+		t.Fatalf("wrong type for `evaluated`. expected=`*object.Array`, actual=`%T`", evaluated)
+	}
+
+	if len(result.Elements) != 3 {
+		t.Fatalf("`result.Elements` has wrong number of elements. expected=`3`, actual=`%d`", len(result.Elements))
+	}
+
+	testIntegerObject(t, result.Elements[0], 1, "1")
+	testIntegerObject(t, result.Elements[1], 4, "2 * 2")
+	testIntegerObject(t, result.Elements[2], 6, "3 + 3")
+}
+
+func TestArrayIndexExpressions(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			"[1,2,3][0]",
+			1,
+		},
+		{
+			"[1,2,3][1]",
+			2,
+		},
+		{
+			"[1,2,3][2]",
+			3,
+		},
+		{
+			"let i = 0; [1][i];",
+			1,
+		},
+		{
+			"[1,2,3][1+1]",
+			3,
+		},
+		{
+			"let myArray = [1,2,3]; myArray[2];",
+			3,
+		},
+		{
+			"let myArray = [1,2,3]; myArray[0]+myArray[1] + myArray[2];",
+			6,
+		},
+		{
+			"let myArray = [1,2,3]; let i = myArray[0]; myArray[i];",
+			2,
+		},
+		{
+			"[1,2,3][3]",
+			nil,
+		},
+		{
+			"[1,2,3][-1]",
+			nil,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, evaluated, int64(integer), tt.input)
+		} else {
+			testNullObject(t, evaluated, tt.input)
+		}
+	}
+}
